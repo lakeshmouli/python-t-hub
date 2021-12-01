6aec6e821aec72b3506ee3458250d301e07b0d0f
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-01 03:54:14 -0800

    Add files via upload

---
+++ balancedprime.py
@@ -0,0 +1,31 @@
+from math import *
+def isprime(num):
+    if num==1:
+        return False
+    sq=int(sqrt(num))
+    for i in range(2,sq+1):
+        if num%i==0:
+            return False
+    return True
+num=int(input())
+if isprime(num):
+    print("prime")
+    np=num+2
+    while True:
+        if isprime(np):
+            break
+        else:
+            np+=2
+    pp=num-2
+    while True:
+        if isprime(pp):
+            break
+        else:
+            pp-=2
+    if np-num==num-pp:
+        print("bal Prime")
+    else:
+        print("Not Bal Prime")
+else:
+    print("not a Prime")
+        
